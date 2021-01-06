279d33614d6a995433f615b73e553d37c92c0562
Andreas Hyldahl <Andreas.Hyldahl10@hotmail.com>
2021-01-06 13:31:21 +0100

    detaljer i homefeed og venners kommentarer indsat i moviedetails

--- app/src/main/java/com/example/yndlingsfilm/MovieDetailsFragment.java
+++ app/src/main/java/com/example/yndlingsfilm/MovieDetailsFragment.java
@@ -1,8 +1,14 @@
 package com.example.yndlingsfilm;
 
+import android.app.Dialog;
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -14,7 +20,10 @@ import android.widget.TextView;
 
 import com.bumptech.glide.Glide;
 import com.example.yndlingsfilm.Model.Movie;
+import com.example.yndlingsfilm.Model.News;
 import com.example.yndlingsfilm.util.Constants;
+
+import java.util.ArrayList;
 
 public class MovieDetailsFragment extends Fragment {
     private static final String TAG = "MovieDetailsFragment";
@@ -28,7 +37,9 @@ public class MovieDetailsFragment extends Fragment {
     Button writeReviewButton;
     TextView movieGenres;
     Movie movie;
-
+    private RecyclerView homeFeed;
+    private HomeFeedAdapter mAdapter;
+    private RecyclerView.LayoutManager layoutManager;
 
 
     @Override
@@ -83,6 +94,54 @@ public class MovieDetailsFragment extends Fragment {
                         R.anim.slide_out_left).replace(R.id.fragment_nagivation, fragment)
                         .addToBackStack(null).commit();
             }
+        });
+
+        final ArrayList<News> aNews = new ArrayList<>();
+        for(int i=0; i<10; i++) {
+            aNews.add(new News(R.drawable.profile_pic, R.drawable.movie_pic,
+                    "Asger Åkanden:", "2hrs", "Harry Potter", 4));
+        }
+
+        homeFeed = (RecyclerView) view.findViewById(R.id.friendsComments);
+        homeFeed.setHasFixedSize(true);
+
+        layoutManager = new LinearLayoutManager(getContext());
+        homeFeed.setLayoutManager(layoutManager);
+
+        mAdapter = new HomeFeedAdapter(aNews);
+        homeFeed.setAdapter(mAdapter);
+
+        mAdapter.setOnItemClickListener(position -> {
+            final Dialog dialog = new Dialog(getContext(), R.style.AnimationDialog);
+            dialog.setContentView(R.layout.popup_news_details);
+            dialog.getWindow().setBackgroundDrawable(new ColorDrawable(Color.TRANSPARENT));
+
+            // sætter popup til at fylde parent
+            //WindowManager.LayoutParams lp = new WindowManager.LayoutParams();
+            //lp.copyFrom(dialog.getWindow().getAttributes());
+            //lp.width = WindowManager.LayoutParams.MATCH_PARENT;
+            //lp.height = WindowManager.LayoutParams.MATCH_PARENT;
+            //dialog.getWindow().setAttributes(lp);
+
+            ImageView moviePic = dialog.findViewById(R.id.moviePic);
+            TextView text = dialog.findViewById(R.id.text);
+            RatingBar rating = dialog.findViewById(R.id.rating);
+            ImageView closeButton = dialog.findViewById(R.id.closeButton);
+
+            moviePic.setImageResource(aNews.get(position).getMoviePicResource());
+            text.setText(aNews.get(position).getTime());
+            rating.setRating(aNews.get(position).getRating());
+
+            closeButton.setOnClickListener(view1 -> dialog.dismiss());
+
+            moviePic.setOnClickListener(view1 -> {
+                //open moviedetails fragment
+                getFragmentManager().beginTransaction().replace
+                        (R.id.fragment_nagivation, new MovieDetailsFragment() )
+                        .addToBackStack(null).commit();
+                dialog.dismiss();
+            });
+            dialog.show();
         });
 
        /* imdbLink.setOnClickListener(view1 -> {
