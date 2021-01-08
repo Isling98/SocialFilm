682db75ba9b0013c1dbfe6645d6fafb68c4eedeb
isling98 <s174879@student.dtu.dk>
2021-01-08 13:15:15 +0100

    Settings layout og logout funktion lavet.

--- app/src/main/java/com/example/yndlingsfilm/NavigationBar/SettingFragment.java
+++ app/src/main/java/com/example/yndlingsfilm/NavigationBar/SettingFragment.java
@@ -1,6 +1,8 @@
 package com.example.yndlingsfilm.NavigationBar;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -9,7 +11,9 @@ import android.widget.ImageButton;
 
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
 
+import com.example.yndlingsfilm.LoginActivity;
 import com.example.yndlingsfilm.PhotoFragment;
 import com.example.yndlingsfilm.R;
 
@@ -20,6 +24,7 @@ public class SettingFragment extends Fragment implements View.OnClickListener {
     EditText editPassword2;
     ImageButton editPasswordConfirm;
     ImageButton deleteConfirm;
+    ImageButton logoutConfirm;
 
     @Nullable
     @Override
@@ -31,10 +36,12 @@ public class SettingFragment extends Fragment implements View.OnClickListener {
         editPassword2 = view.findViewById(R.id.editPass2);
         editPasswordConfirm = view.findViewById(R.id.editPassConfirm);
         deleteConfirm = view.findViewById(R.id.deleteConfirm);
+        logoutConfirm = view.findViewById(R.id.logOutConfirm);
 
         editProfilePic.setOnClickListener(this);
         editPasswordConfirm.setOnClickListener(this);
         deleteConfirm.setOnClickListener(this);
+        logoutConfirm.setOnClickListener(this);
 
 
 
@@ -46,14 +53,48 @@ public class SettingFragment extends Fragment implements View.OnClickListener {
     @Override
     public void onClick(View v) {
 
-        if (v.getId() == R.id.editProfilePicButton){
+        switch (v.getId()) {
+            case R.id.editProfilePicButton:
+                Fragment selectedFragment = new PhotoFragment();
+                getFragmentManager().beginTransaction()
+                        .setCustomAnimations(R.anim.slide_in_right, R.anim.slide_out_left)
+                        .replace(R.id.fragment_nagivation, selectedFragment)
+                        .addToBackStack(null)
+                        .commit();
+                break;
+            case R.id.editPassConfirm:
+                System.out.println("editpass virker");
+                break;
+            case R.id.logOutConfirm:
+
+                FragmentManager fragmentManager = getFragmentManager();
+                fragmentManager.popBackStack(null, FragmentManager.POP_BACK_STACK_INCLUSIVE);
+
+                Intent intent = new Intent(getActivity(), LoginActivity.class);
+                startActivity(intent);
+//                getActivity().overridePendingTransition(R.anim.slide_up, R.anim.slide_down);
+
+                getActivity().finish();
+                //Nedenstående bruges til at fjerne alle fragments så der ikke kan klikkes 'tilbage' efter logout.
+
+
+
+                break;
+            case R.id.deleteConfirm:
+                System.out.println("delete virker");
+                break;
+        }
+
+        /*if (v.getId() == R.id.editProfilePicButton){
             Fragment selectedFragment = new PhotoFragment();
             getFragmentManager().beginTransaction()
                     .setCustomAnimations(R.anim.slide_in_right, R.anim.slide_out_left)
                     .replace(R.id.fragment_nagivation, selectedFragment)
                     .addToBackStack(null)
                     .commit();
-        }
+        }*/
+
+
 
 
         /*
